5590cf8c4348b1546671df9f2224ac6a56d2d888
Pedro Gonçalo Correia <goncalerta@gmail.com>
2019-11-24 00:12:50 +0000

    Add more assets

--- bomberman.py
+++ bomberman.py
@@ -17,8 +17,18 @@ class Block(Enum):
     BOX_GOAL = 3
     GOAL = 4
 
+    def draw(self, ctx, x, y):
+        assets_indexes = {
+            Block.GRASS: 'grass',
+            Block.WALL: 'wall',
+            Block.BOX: 'box',
+            Block.BOX_GOAL: 'box',
+        }
+        ctx['screen'].blit(ctx['assets'][assets_indexes[self]], (x, y))
+
+
 class Level:
-    def __init__(self, matrix = None):
+    def __init__(self, matrix = None, goal = None):
         if matrix == None:
             self.matrix = [
                 [1 for _ in range(13)],
@@ -35,32 +45,45 @@ class Level:
                 [i == 0 or i == 12 for i in range(13)],
                 [1 for _ in range(13)],
             ]
+
+            self.matrix = [[Block(i) for i in row] for row in self.matrix]
         else:
             self.matrix = matrix
+
+        if goal != None:
+            x, y = goal
+            self.matrix[y][x] = Block.BOX_GOAL 
 
     def draw(self, ctx):
         for i, row in enumerate(self.matrix):
             for j, block in enumerate(row):
-                ctx['screen'].blit(ctx['entities'][Block(block)], (j*50, i*50))
+                block.draw(ctx, j*50, i*50)
 
-    
+    def is_solid(self, x, y):
+        self.matrix[y][x] in [Block.WALL, Block.BOX, Block.BOX_GOAL]
+
+
 
 def init():
     size = width, height = 650, 650
     speed = [2, 2]
     
     screen = pygame.display.set_mode(size)
-    entities = {
-        Block.GRASS: pygame.image.load("assets/grass.png"),
-        Block.WALL: pygame.image.load("assets/wall.png")
+    assets = {
+        'grass': pygame.image.load('assets/grass.png'),
+        'wall': pygame.image.load('assets/wall.png'),
+        'box': pygame.image.load('assets/box.png'),
+        'goal': pygame.image.load('assets/goal.png'),
+        'player': pygame.image.load('assets/player.png'),
+        'bomb': pygame.image.load('assets/bomb.png'),
     }
 
-    level = Level()
+    level = Level(goal = (5, 3))
 
     context = {
         'size': size,
         'speed': speed,
-        'entities': entities,
+        'assets': assets,
         'screen': screen,
     }
     
